1ba6c70581d601ccb8b2e1dc32b042685120f9d9
Jonathan Chute <jochute@gmail.com>
2015-12-02 11:58:36 -0400

    Created expand.js
    Initial Release.

---
+++ expand.js
@@ -0,0 +1,57 @@
+/*
+  Version: 1.0
+  Developer: Jonathan Chute
+  Year: 2015
+*/
+
+(function( $ ) {
+    $.fn.expand = function(options) {
+        
+        var mainObj = $(this);
+        
+        if(mainObj[0] === undefined)
+            return;
+		
+        var settings = $.extend( {
+            'wrapper': 'body',
+            'end':   ''
+        }, options);
+        
+        $(window).on("load resize", function(){
+            setHeight();
+        });
+        
+        function setHeight() {
+            var height = $(window).height();
+
+            height -= subtract('html', true);
+            height -= subtract('body', true);
+            height -= subtract(settings.wrapper, true);
+            height -= subtract(mainObj, false);
+            
+            $(settings.wrapper).children().not(mainObj).each(function(){
+                height -= $(this).outerHeight(true);
+                console.log($(this));
+                console.log(height);
+                if(settings.end != '') if($(this).is(settings.end)) return false;
+            });
+            
+            $(mainObj).css('min-height', height);
+        }
+        
+        function subtract(object, padding) {
+            var total = 0;
+            
+            total += parseInt($(object).css('marginTop'));
+            total += parseInt($(object).css('marginBottom'));
+            
+            if(padding) {
+                total += parseInt($(object).css('paddingTop'));
+                total += parseInt($(object).css('paddingBottom'));
+            }
+            
+            return total;
+        }
+        
+    };
+})( jQuery );
