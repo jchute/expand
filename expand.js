6c0187b4efad68e0564b91222a95a4ceab23c780
Jonathan Chute <jochute@gmail.com>
2015-12-03 15:08:55 -0400

    Update expand.js

--- expand.js
+++ expand.js
@@ -7,20 +7,22 @@
 
 (function( $ ) {
     $.fn.expand = function(options) {
-		
+
         var settings = $.extend( {
             'end':   ''
         }, options);
-        
+
         var mainObj = $(this);
             if(mainObj[0] === undefined)
                 return;
         var wrapper = mainObj.parent();
-        
+
+        setHeight();
+
         $(window).on("load resize", function(){
             setHeight();
         });
-        
+
         function setHeight() {
             var height = $(window).height();
 
@@ -28,30 +30,30 @@
             height -= subtract('body', true);
             height -= subtract(wrapper, true);
             height -= subtract(mainObj, false);
-            
+
             $(wrapper).children().not(mainObj).each(function(){
                 height -= $(this).outerHeight(true);
                 console.log($(this));
                 console.log(height);
                 if(settings.end != '') if($(this).is(settings.end)) return false;
             });
-            
+
             $(mainObj).css('min-height', height);
         }
-        
+
         function subtract(object, padding) {
             var total = 0;
-            
+
             total += parseInt($(object).css('marginTop'));
             total += parseInt($(object).css('marginBottom'));
-            
+
             if(padding) {
                 total += parseInt($(object).css('paddingTop'));
                 total += parseInt($(object).css('paddingBottom'));
             }
-            
+
             return total;
         }
-        
+
     };
 })( jQuery );
