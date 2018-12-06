0090e194f2440c8a71c6f9c0797fa3094a1786b2
Jonathan Chute <jochute@gmail.com>
2018-12-06 16:13:56 -0400

    Update expand.js
    - Added in Formatting
    - Added in a "wrapper" argument so that you can define which tag to treat as the wrapping div - it defaults to body
    - Added in a "section" argument so that you can choose to only apply your check to certain child sections rather than all child tags within the wrapper
    - Renamed "end" to "last" to better represent it's purpose.

--- expand.js
+++ expand.js
@@ -1,71 +1,110 @@
 /*
   Expand - Short Page Fix
-  Version: 1.3
+  Version: 1.4
   Developer: Jonathan Chute
-  Year: 2017
+  Year: 2018
 */
 (function( $ ) {
     $.fn.expand = function(options) {
 
         var settings = $.extend( {
-            'end':   '',
+            'sections': 'div',
+            'last':   '',
+            'wrapper': 'body',
             'after': false,
-        }, options);
+        }, options );
 
         var mainObj = $(this);
-            if(mainObj[0] === undefined)
-                return;
-        var wrapper = mainObj.parent();
 
-        setHeight();
-
-        $(window).on("load resize", function(){
-            setHeight();
-            if(settings.after != false) {
-                settings.after();
-            }
-        });
-
-        function setHeight() {
-            var height = $(window).height();
-
-            height -= subtract('html', true);
-            height -= subtract('body', true);
-            height -= (!compare($('body'), wrapper)) ? subtract(wrapper, true) : 0;
-            height -= subtract(mainObj, false);
-
-            $(wrapper).children().not(mainObj).each(function(){
-                var position = $(this).css('position');
-                if(position == 'absolute' || position == 'fixed') return true;
-                height -= $(this).outerHeight(true);
-                if(settings.end != '') if($(this).is(settings.end)) return false;
-            });
-
-            $(mainObj).css('min-height', height);
+        if ( mainObj[0] === undefined ) {
+            return;
         }
 
-        function subtract(object, padding) {
+        settings.sections = $(settings.wrapper).children( settings.sections );
+            
+
+        setHeight(); setInterval( setHeight, 400 );
+
+        function setHeight() {
+
+            var height = $(window).height();
+
+            height -= subtract( 'html', true );
+            height -= subtract( 'body', true );
+            height -= ( ! compare( $('body'), $(settings.wrapper) ) ) ? subtract( $(settings.wrapper), true ) : 0;
+            height -= subtract( mainObj, false );
+
+            settings.sections.not( mainObj ).each( function() {
+
+                var position = $(this).css( 'position' );
+
+                if ( position == 'absolute' || position == 'fixed' ) {
+
+                    return true;
+
+                }
+
+                height -= $(this).outerHeight( true );
+
+                if ( settings.last != '' ) {
+
+                    if ( $(this).is( settings.last ) ) {
+
+                        return false;
+
+                    }
+
+                }
+
+            } );
+
+            $(mainObj).css( 'min-height', height );
+
+            if ( settings.after != false ) {
+
+                settings.after();
+
+            }
+        }
+
+        function subtract( object, padding ) {
+
             var total = 0;
 
-            total += parseInt($(object).css('marginTop'));
-            total += parseInt($(object).css('marginBottom'));
-            total += parseInt($(object).css('borderTopWidth'));
-            total += parseInt($(object).css('borderBottomWidth'));
+            total += parseInt( $(object).css( 'margin-top' ) );
+            total += parseInt( $(object).css( 'margin-bottom' ) );
+            total += parseInt( $(object).css( 'border-top-width' ) );
+            total += parseInt( $(object).css( 'border-bottom-width' ) );
 
-            if(padding) {
-                total += parseInt($(object).css('paddingTop'));
-                total += parseInt($(object).css('paddingBottom'));
+            if ( padding ) {
+
+                total += parseInt($(object).css('padding-top'));
+                total += parseInt($(object).css('padding-bottom'));
+
             }
 
             return total;
+
         }
         
-        function compare(object1, object2) {
-            if(object1.length !== object2.length) return false;
-            for(var i = 0; i < object1.length; i++ ) {
-                if(object1[i] !== object2[i]) return false;
+        function compare( object1, object2 ) {
+
+            if ( object1.length !== object2.length ) {
+
+                return false;
+
             }
+
+            for ( var i = 0; i < object1.length; i++ ) {
+
+                if ( object1[i] !== object2[i] ) {
+                    return false;
+                }
+
+            }
+
             return true;
+
         }
 
     };
